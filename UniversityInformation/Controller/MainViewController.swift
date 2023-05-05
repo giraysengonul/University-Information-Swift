a07c59eded34b283ef963aaa6f20dd54e34858bd
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-05-05 16:32:10 +0300

    Setup

---
+++ UniversityInformation/Controller/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  UniversityInformation
+//
+//  Created by hakkı can şengönül on 5.05.2023.
+//
+
+import UIKit
+class MainViewController: UIViewController {
+     // MARK: - Properties
+     // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+ // MARK: - Helpers
